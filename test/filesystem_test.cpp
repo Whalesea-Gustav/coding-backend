d6f707017bb875ead2569f733c499e5d15d76d81
Jim <40590729+Whalesea-Gustav@users.noreply.github.com>
2024-03-06 19:22:49 +0800

    Add std::filesystem test

---
+++ test/filesystem_test.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#include <filesystem>
+
+using namespace std;
+
+
+int main()
+{
+    filesystem::path  p("E:/CrowdDetection/revaluate/APMRToolkits.joke");
+    std::cout<<"Root Dir:"<<p.root_directory() << std::endl;
+    std::cout << "Root Name:" << p.root_name() << std::endl;
+    std::cout << "Root Path:" << p.root_path() << std::endl;
+    std::cout << "File Name Without Extension:" << p.stem() << std::endl;
+    std::cout << "File Extension:" << p.extension() << std::endl;
+    std::cout << "File Name :" << p.filename() << std::endl;
+    std::cout << "Relative Dir:" << p.relative_path() << std::endl;
+    std::cout << "Parent Dir:" << p.parent_path()<< std::endl;
+
+    p.replace_extension("jpg");
+
+    std::cout << "Full String:" << p.string() << std::endl;
+
+    p.replace_filename("hahahah");
+
+    std::cout << "Full String:" << p.string() << std::endl;
+
+   return 0;
+}
